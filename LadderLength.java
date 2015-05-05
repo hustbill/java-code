0603187b6a4492d2ad3fbda3b00f7cf13fb1e605
hua.zhang <hua.zhang@email.wsu.edu>
2015-05-04 19:25:25 -0700

    update Word Ladder

--- LadderLength.java
+++ LadderLength.java
@@ -30,49 +30,93 @@ public class LadderLength {
     public static int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
 		int len = beginWord.length();
 		String interWord = beginWord;
-		int count =0; // the number of same letters between interWord and endWord
-		Set<String> result = new HashSet<String>();
-		result.add(beginWord);
+	
+		
+		//Set<String> result = new HashSet<String>(wordDict.size());
+		ArrayList<String> result = new ArrayList<String>();
+		//result.add(beginWord);
 		
 		if ( endWord.length() != len) 
 			return 0;
 		
-        for( int i=0; i< len; i++) {
-			for( int j=0; j< len; j++) {
-				//System.out.print(beginWord.charAt(i) + ", " + endWord.charAt(j) + " count=" + count +  " ");
-		        interWord = beginWord.replace( beginWord.charAt(i), endWord.charAt(j));
-					
-					
-					if( wordDict.contains(interWord)) {  // interWord is in wordDict
-						System.out.println(interWord);
-						result.add(interWord);
-						ladderLength(interWord, endWord, wordDict);
-						count++;
-						
-						if(count == len -1 ) {
-							System.out.println(result);
-							return result.size();
+		if( beginWord.regionMatches(0, endWord, 0, len-1) ||
+	  		beginWord.regionMatches(0, endWord, 1, len-1) ||
+			beginWord.regionMatches(1, endWord, 0, len-1) ||
+			beginWord.regionMatches(1, endWord, 1, len-1)
+	   	) {
+		   result.add(beginWord);
+		   result.add(endWord);
+		   return result.size();
+		   
+	  	} else {
+
+			System.out.println(wordDict);
+			 Iterator itr = wordDict.iterator(); 
+			 while(itr.hasNext()) {
+	 	     		Object obj = itr.next(); 
+					String element = obj.toString();
+					//System.out.println(element + " " + interWord);
+				
+				    if(  !interWord.equals(element) && 
+						( interWord.regionMatches(0, element, 0, len-1) ||
+				  		interWord.regionMatches(0, element, 1, len-1) ||
+						interWord.regionMatches(1, element, 0, len-1) ||
+						interWord.regionMatches(1, element, 1, len-1)
+				   	)) {
+						//System.out.println("interWord= " +interWord);
+						 interWord = element;
+						  //System.out.println("element= " +element);
+						 //ladderLength(interWord, endWord, wordDict);
+						 
+				 		if( interWord.regionMatches(0, endWord, 0, len-1) ||
+				 	  		interWord.regionMatches(0, endWord, 1, len-1) ||
+				 			interWord.regionMatches(1, endWord, 0, len-1) ||
+				 			interWord.regionMatches(1, endWord, 1, len-1)
+				 	   	) {
+				 		   result.add(interWord);
+				 		   result.add(endWord);
+						   System.out.println("result 1 =" + result);
+				 		   return result.size() +1;
+		   
+				 	  	}
+						 
+			  		} else {
+						int count =0; // the number of same letters between interWord and endWord
+						for(int i=0; i< len; i++) {
+							for(int j=0; j< len; j++) {
+								////System.out.println(interWord.charAt(i) == element.charAt(j));
+								if(interWord.charAt(i) == element.charAt(j)){
+								
+									count++;
+									//System.out.println("count=" + count);
+								}
+							}
 						}
-					}
-					 // else{
-	 // 						System.out.println(" interWord is not in wordDict");
-	 //
-	 //
-	 // 					}
-				
-				
-				 // else {
-	 // 					ladderLength(interWord, endWord, wordDict);
-	 // 				}
-        	}
-        }
-		
-		return result.size();
+						if(count == len-1) {  // hit -> hot
+							interWord = element;
+								result.add(interWord);
+							 System.out.println("interWord= " +interWord);
+							ladderLength(interWord, endWord, wordDict);
+							
+						}
+						
+			  		}
+				} 
+				//System.out.println();
+	  	  }
+		  System.out.println("result =" + result);
+		  int size = result.size() +1;
+		  return size;
     }
 	
 	public static void main(String[] args) {
-		String start = "hit", end = "cog";
-		String[] dict = {"hot","dot","dog","lot","log"};
+		String start = "hit";
+		String end = "dog";
+		 String[] dict = {"hot","dot","dog","lot","log"};
+		
+		// String end = "cog";
+			//String[] dict = {"hot","hit","cog","dot","dog"};
+		
 		Set<String> wordDict = new HashSet<String>();
 		for(int i=0; i< dict.length; i++) {
 			wordDict.add(dict[i]);
