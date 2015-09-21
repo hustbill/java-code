f8c24f38efe10904f79c5129a6b3dd935b28f0af
Hua Zhang <billyzhang2010@gmail.com>
2015-09-21 09:16:23 -0700

    generate palindormes

---
+++ Solution.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+
+    public static void findCelebrity() {
+
+        
+     }
+     
+     
+     public static void main(String args) {
+         
+     
+     }
+     
+     public boolean knows(int a, int b) {
+         
+         if (a> b)  return true;
+         return false;
+     }
+}
+
+//https://www.hackerrank.com/
+
