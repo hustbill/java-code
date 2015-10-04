17360e398eeb4e37e3831a06f3dbb15412af4aa7
Hua Zhang <billyzhang2010@gmail.com>
2015-10-03 22:59:22 -0700

    extend to k iterators

--- ZigzagIterator.java
+++ ZigzagIterator.java
@@ -1,42 +1,32 @@
-
 import java.util.*;
 
 public class ZigzagIterator {
 
-	private int index=0, size;
-	private ArrayList<Integer> al = new ArrayList<Integer>();
-	
+    private Iterator<Integer> itr1, itr2, tmp;
+    
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-	
-    	if (v1.size()!=0 && v2.size() !=0)  {
-			int min = Math.min(v1.size(), v2.size());
-			int max = Math.max(v1.size(), v2.size());
-			for (int i=0; i< min; i++) {
-				al.add(v1.get(i));
-				al.add(v2.get(i));
-			}
-			for(int j=min; j< max; j++) {
-				if( v1.size() < v2.size()){
-					al.add(v2.get(j)); 
-				} else {
-					al.add(v1.get(j));
-				}
-			}
-    	} else {
-    		if ( v1.size() == 0  && v2.size() !=0   )	
-	    		al = new ArrayList<Integer>(v2);
-    	    if ( v1.size() != 0 &&  v2.size() == 0 )  
-    	    	al = new ArrayList<Integer>(v1);
-    	}
-    	size = al.size();
+	    itr1 = v1.iterator();
+	    itr2 = v2.iterator();
     }
 
     public int next() {
-        return al.get(index-1);
+        if (itr1.hasNext())  { 
+            tmp=itr2;
+            itr2=itr1;
+            itr1=tmp;
+        }
+        return itr2.next();
     }
 
     public boolean hasNext() {
-		return index++ < size ? true : false;
+        return itr1.hasNext() || itr2.hasNext();
     }
 
+
 }
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
