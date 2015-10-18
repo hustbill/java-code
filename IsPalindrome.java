e56dd8846c14052c84739d3a93b15fa7a0dc78db
Hua Zhang <billyzhang2010@gmail.com>
2015-10-18 10:15:19 -0700

    IsPalindrome, without extra space

---
+++ IsPalindrome.java
@@ -0,0 +1,24 @@
+public class IsPalindrome {
+    public static boolean isPalindrome(int x) {
+        int p=x, q=0;
+		
+		while(p>=10) {
+			q *=10;   // q=0,  q=10  q=120
+			q += p%10;  // if x=1221, 1221%10 => q = 1,  122%10=2 q=12   q=120+2=122
+			p /=10;	    //  p/10 => p= 122,    p/10 => p=12  p =1
+		}
+		return (p == x%10 && q == x/10);
+		
+    }
+	
+	public static void main(String[] args) {
+		int x = 121;
+		int y = 1221;
+		int z = 12321;
+		
+		int[] arr = {x, y,z};
+		for( int i : arr) { 
+			System.out.println(isPalindrome(i));
+		}
+	}
+}
