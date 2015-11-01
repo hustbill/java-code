26fe575898141b4783a36d18da3761cabdade782
Hua Zhang <hua.zhang@wsu.edu>
2015-11-01 10:04:49 -0800

    update Merge Intervals

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -7,17 +7,39 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
- 
+ import java.util.*;
  
 public class MergeIntervals {
-    public List<Interval> merge(List<Interval> intervals) {
-        
+    public static List<Interval> merge(List<Interval> intervals) {
+        return intervals;
     }
+	
+	public static void main(String[] args) {
+		/* 
+		  Given [1,3],[2,6],[8,10],[15,18],
+		  return [1,6],[8,10],[15,18].
+		*/
+		Interval x = new Interval( 1,3);
+		Interval y = new Interval( 2,6);
+		Interval z = new Interval( 8,10);
+		Interval r = new Interval( 15,18);
+		
+		List<Interval> lst = new ArrayList<Interval>();
+		lst.add(x);
+		lst.add(y);
+		lst.add(z);
+		lst.add(r);
+		
+		List<Interval> res = merge(lst);
+		
+		for( Interval i : res) 
+			System.out.println( "[" +  i.start + ", " + i.end + "]"\);
+	}
 }
 
 class Interval {
 	int start;
 	int end;
 	Interval() { start = 0; end =0;}
-	Interval(int s, int e) { star = s ; end = e; }
+	Interval(int s, int e) { start = s ; end = e; }
 }
