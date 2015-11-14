786588732552a0044b30b074e1184cd4cf95ce5a
Hua Zhang <billyzhang2010@gmail.com>
2015-11-14 11:55:46 -0800

    DP: find ordering of matrix multiplications

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,9 +16,22 @@ public class Fibonacci{
         return answer;
     }
     
+    // evaluate the recursion solution for Fibonacci
+    public static double eval (int n) {
+        if ( n == 0) 
+            return 1.0;
+        else {
+            double sum = 0.0;
+            for ( int i = 0; i < n; i++) 
+                sum += eval( i );
+            return 2.0 * sum / n  + n;
+        }
+    }
     
     public static void main(String[] args) {
         fibonacci(5);
+        int sum = (int)eval(5);
+        System.out.printf( "sum = %d", sum);
     }
 
 }
