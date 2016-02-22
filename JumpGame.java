44607f2792129a2d24c3c9dec138d82304a04ebc
Hua Zhang <hua.zhang@wsu.edu>
2016-02-22 15:23:27 -0800

    JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,27 @@
+/*55. Jump Game
+Total Accepted: 68926 Total Submissions: 247941 Difficulty: Medium
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+
+Hide Company Tags Microsoft
+Hide Tags Array Greedy
+*/
+
+public class JumpGame {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public boolean canJump(int[] nums) {
+         
+    }
+}
