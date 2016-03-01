71d881ff1efc4e6994dd7cd012491a3324d10c6d
Hua Zhang <hua.zhang@wsu.edu>
2016-02-29 18:00:40 -0800

    update LevelOrder BFS and DFS

--- LevelOrder.java
+++ LevelOrder.java
@@ -1,5 +1,6 @@
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        // 1. bfs - using queue https://leetcode.com/discuss/22533/java-solution-with-a-queue-used
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
 
@@ -18,4 +19,25 @@ public class Solution {
         }
         return wrapList;
     }
+    
+    public List<List<Integer>> levelOrderDFS(TreeNode root) {
+            // 2. dfs -https://leetcode.com/discuss/21778/java-solution-using-dfs
+            List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+            levelHelper(res, root, 0);
+        
+            return res;
+        }
+    
+        public void levelHelper( List<List<Integer>> res,  TreeNode root, int height) {
+            if (root == null)  return ;
+        
+            if (height >= res.size())  {
+                res.add(new LinkedList<Integer>());
+            }
+            res.get(height).add(root.val);
+        
+            if(root.left != null)  levelHelper(res, root.left, height+1);
+            if(root.right != null) levelHelper(res, root.right, height+1);
+        }
 }
