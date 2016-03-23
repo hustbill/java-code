9979004ae8428bf8f24eeeb75ce9ab01f34d2b3d
Hua Zhang <hua.zhang@wsu.edu>
2016-03-23 12:41:18 -0700

    rename learn-java to headfirst-java

--- ReverseWordsInString.java
+++ ReverseWordsInString.java
@@ -1,24 +1,42 @@
+/*
+151. Reverse Words in a String My Submissions Question
+Total Accepted: 95234 Total Submissions: 608927 Difficulty: Medium
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Update (2015-02-12):
+For C programmers: Try to solve it in-place in O(1) space.
+
+click to show clarification.
+
+Hide Company Tags Bloomberg
+Hide Tags String
+Hide Similar Problems (M) Reverse Words in a String II
+*/
+
+
 import java.util.*;
 
 public class ReverseWordsInString{
 	public static String reverseWordsInString(String s) {
-		if (s == null || s.length() <=1) return s;
-		StringBuilder sbr = new StringBuilder(); 
-		int i = s.length()-1;
-		int j= s.length()-1;
+		StringBuilder res = new StringBuilder(); 
+		int j = s.length();
 		
-			
-		while(i <=j && i>=0 ) {
-			//System.out.println("i: " + s.charAt(i));
-			if (Character.isWhitespace(s.charAt(i)) || i==0 ) {
-				sbr.append(s.substring( i, j));
-				j=i+1;
-			}
-			i--;
-			// System.out.println(sbr.toString());
-		}
-		
-		return sbr.toString();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                j = i;
+            } else if (i == 0 || s.charAt(i-1) == ' ') {
+                
+                if (res.length() != 0) {
+                    res.append(' ');
+                }
+                res.append(s.substring(i,j));
+            }
+        }
+        return res.toString();
 	}
 	
 	public static void main(String[] args) {
--- learn-java/README.md
+++
@@ -1,6 +0,0 @@
-#Introduction to Programming in Java 
-
-
-http://introcs.cs.princeton.edu/java/home/?utm_medium=email&utm_source=other&utm_campaign=notifications.auto.m0V9Lt8jEeWQIwqCEOt5aQ
-
-
