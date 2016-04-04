25061ac05025a481e181756c26dd6da0d27d1169
Hua Zhang <hua.zhang@wsu.edu>
2016-04-03 21:33:00 -0700

    StrStr

--- StrStr.java
+++ StrStr.java
@@ -40,7 +40,7 @@ public class StrStr {
             for (int j=0; ; j++) {
                 if (j == needle.length()) return i;
                 if (i + j == haystack.length()) return -1;
-                if (needle.charAt(j) != haystack.charAt(i+j)) break;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
             
             }
         }
