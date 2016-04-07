2bb1b1a8ccddbfeacae839b2ab6a1fa6e9c74ac3
Hua Zhang <hua.zhang@wsu.edu>
2016-04-07 15:19:15 -0700

    stack test

--- common-data-structure/MyStackTest.java
+++ common-data-structure/MyStackTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MyStackTest {
+public class {
 
   @Before
   public void setUp() throws Exception {
