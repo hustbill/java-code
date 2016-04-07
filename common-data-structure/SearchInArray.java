3d82a43d8869cdfddf4bc5920755135f2984b63e
Hua Zhang <hua.zhang@wsu.edu>
2016-04-07 15:36:53 -0700

    add binary search

--- common-data-structure/SearchInArray.java
+++ common-data-structure/SearchInArray.java
@@ -6,9 +6,38 @@ public class SearchInArray {
         Integer[] nums = {1 , 9, 12 , 2, 3, 8, 6, 7, 4 , 10 ,5 ,11};
         List<Integer> list =  new ArrayList<Integer>();
         list = Arrays.asList(nums);
-        System.out.print(mergeSort(list));
+        
+        List<Integer> sortedList = mergeSort(list);
+        System.out.print(sortedList);
+        
+        // binary search for a target in sorted array
+        int target = 11;
+        System.out.printf("\nFind a target %d in sorted list\n", target);
+        System.out.println(binarySearch(sortedList, target));
+        
+    }
+    // 
+    public static boolean binarySearch(List<Integer> list, int target) {
+        if (list == null || list.isEmpty()) {
+            return false;
+        }
+        
+        final int mid = list.size() / 2;
+        final int comparison = list.get(mid);
+        
+        if (target == comparison) {
+            return true;
+        }
+        
+        if (target < comparison) {
+            return binarySearch(list.subList(0, mid), target);
+        } else {
+            return binarySearch(list.subList(mid, list.size()), target);
+        }
+        
     }
     
+    // Time complexity - O(nlogn)
     public static List<Integer> mergeSort(List<Integer> list) {
         if (list.size() < 2) {
             return list;
@@ -21,6 +50,7 @@ public class SearchInArray {
         return merge(mergeSort(leftList), mergeSort(rightList)); 
     }
     
+    // O(N)
     public static List<Integer> merge(List<Integer> l,  List<Integer> r) {
         int leftPtr = 0, rightPtr = 0;        
         List<Integer> toReturn = new ArrayList<>();
