b5325aec7b4ed8025ddd73cfaa6f651f35491f40
Hua Zhang <hua.zhang@wsu.edu>
2016-04-24 10:27:40 -0700

    using bit manuplication  n & (n-1) == 0

--- src/v201604/IsPowerOfTwo_231.java
+++ src/v201604/IsPowerOfTwo_231.java
@@ -6,17 +6,37 @@ public class IsPowerOfTwo_231 {
 		// TODO Auto-generated method stub
 		int n = 128;
 		
-		System.out.print(isPowerOfTwo(n));
+		System.out.println(isPowerOfTwo(n));
+		
+		n = 256 *8 * 3;
+		System.out.println(isPowerOfTwo(n));
+		
+		n = 1;
+		System.out.println(isPowerOfTwo(n));
+		
+		n = -16;
+		System.out.println(isPowerOfTwo(n));
+		
+		
+		
 	}
 	
-	 public static boolean isPowerOfTwo(int n) {
-	        while (n > 0) {
-	            n = n/2;
-	        }
-	        if ( n %2 != 0) {
-	            return false;
-	        }
-	        return true;
-	 }
+    public static boolean isPowerOfTwo(int n) {
+        /* https://leetcode.com/discuss/43875/using-n%26-n-1-trick
+          Ref: Power of 2 means only one bit of n is '1', so use the trick n&(n-1)==0 to judge whether that is the case
+        */
+        return (n > 0 && (n & (n-1)) == 0 );
+    
+    }
+    public static boolean isPowerOfTwo_mine(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+      
+        
+       	while ( n > 2 &&  n % 2 == 0)  {
+ 			n = n / 2;
+ 		}
+ 		return n % 2 == 0; 
+    }
 
 }
--- src/v201604/IsPowerOfTwo_231Test.java
+++ src/v201604/IsPowerOfTwo_231Test.java
@@ -19,7 +19,19 @@ public class IsPowerOfTwo_231Test {
   public void isPowerOfTwo() {
     //throw new RuntimeException("Test not implemented");
 	  IsPowerOfTwo_231 obj = new IsPowerOfTwo_231();
-	  boolean result = obj.isPowerOfTwo(128);
-	  Assert.assertEquals(result, true);
+	  int[] nums = {128, 1};
+	  for (int n : nums) {
+		  boolean result = obj.isPowerOfTwo(n);
+		  Assert.assertEquals(result, true);
+	  }
+	  
+	  int[] failedNums = { 1280, -16};
+	  
+	  for (int n : failedNums) {
+		  boolean result = obj.isPowerOfTwo(n);
+		  Assert.assertEquals(result, false);
+	  }
+	  
+	  
   }
 }
