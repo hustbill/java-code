015e624eac7e5e3616ecee2fed950b6d1c078c19
Hua Zhang <hua.zhang@wsu.edu>
2016-05-02 23:03:49 -0700

    src/v201605/IsPalindrome_234.java

---
+++ src/v201605/IsPalindrome_234.java
@@ -0,0 +1,27 @@
+/*
+234. Palindrome Linked List
+
+Total Accepted: 46880 Total Submissions: 166483 Difficulty: Easy
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+Hide Company Tags Amazon Facebook
+Hide Tags Linked List Two Pointers
+Hide Similar Problems (E) Palindrome Number (E) Valid Palindrome (E) Reverse Linked List
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class IsPalindrome_234 {
+    public boolean isPalindrome(ListNode head) {
+        
+    }
+}
