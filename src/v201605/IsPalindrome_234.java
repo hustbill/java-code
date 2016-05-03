edf4729d57ae022c02a2fa0db11c69ba89b9f3aa
Hua Zhang <hua.zhang@wsu.edu>
2016-05-02 23:19:25 -0700

    src/v201605/IsPalindrome_234.java

--- src/v201605/IsPalindrome_234.java
+++ src/v201605/IsPalindrome_234.java
@@ -22,6 +22,40 @@ Hide Similar Problems (E) Palindrome Number (E) Valid Palindrome (E) Reverse Lin
  */
 public class IsPalindrome_234 {
     public boolean isPalindrome(ListNode head) {
-        
+        // ref" https://leetcode.com/discuss/45656/easy-understand-java-solution-o-1-space-cost
+        if(head == null) {
+                   return true;
+               }
+               ListNode p1 = head;
+               ListNode p2 = head;
+               ListNode p3 = p1.next;
+               ListNode pre = p1;
+               //find mid pointer, and reverse head half part
+               while(p2.next != null && p2.next.next != null) {
+                   p2 = p2.next.next;
+                   pre = p1;
+                   p1 = p3;
+                   p3 = p3.next;
+                   p1.next = pre;
+               }
+
+               //odd number of elements, need left move p1 one step
+               if(p2.next == null) {
+                   p1 = p1.next;
+               }
+               else {   //even number of elements, do nothing
+
+               }
+               //compare from mid to head/tail
+               while(p3 != null) {
+                   if(p1.val != p3.val) {
+                       return false;
+                   }
+                   p1 = p1.next;
+                   p3 = p3.next;
+               }
+               return true;
+
+           }
     }
 }
