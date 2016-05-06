2ce225822953294deee471dad71525f4449727e6
Hua Zhang <hua.zhang@wsu.edu>
2016-05-06 00:29:28 -0700

    calc the run time to compare two rounds. Using cache has better performance.

--- NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/App.java
+++ NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/App.java
@@ -8,7 +8,26 @@ import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 import com.twilio.sdk.TwilioRestException;
 
 /**
- * Main
+1st round Validated Cell Phone Numbers:
+3605532393 
+2025550104 
+2025550197 
+2025550156 
+2025550142 
+2025550117 
+2025550169 
+Run time:  5988ms
+
+2nd round Validated Cell Phone Numbers:  Using LRU Cache
+3605532393 
+2025550104 
+2025550197 
+2025550156 
+2025550142 
+2025550117 
+2025550169 
+Run time:  5145ms
+
  *
  */
 public class App {
@@ -17,20 +36,60 @@ public class App {
 	        List<String> phoneNumbers = new ArrayList<String>();
 	        phoneNumbers.add("360-553-2393");  // MOBILE
 	        phoneNumbers.add("408.441.8300");  // LANDLINE
-	   
+	        
+	    
+	        phoneNumbers.add("202-555-0142");
+	        phoneNumbers.add("202-555-0104");
+	        phoneNumbers.add("202-555-0197");
+	        phoneNumbers.add("202-555-0156");
+	        phoneNumbers.add("202-555-0117");
+	        phoneNumbers.add("202-555-0169");
+	        
+
+	    
 	        //Invalid phone numbers
 	        phoneNumbers.add("(123) 456 7890");
 	        phoneNumbers.add("555-541-1234");
 	        phoneNumbers.add("12-12-111");
 	        phoneNumbers.add("280 456 7890");
+	        
+	        List<String> phoneNumbers2 = new ArrayList<String>();
+	        phoneNumbers2.add("360-553-2393");  // MOBILE
+	        phoneNumbers2.add("408.441.8300");  // LANDLINE
+	        
+	    
+	        phoneNumbers2.add("202-555-0142");
+	        phoneNumbers2.add("202-555-0104");
+	        phoneNumbers2.add("202-555-0197");
+	        phoneNumbers2.add("202-555-0156");
+	        phoneNumbers2.add("202-555-0117");
+	        phoneNumbers2.add("202-555-0169");
 
-	        System.out.println("Validated Cell Phone Numbers:");
+	        System.out.println("1st round Validated Cell Phone Numbers:");
+	        long startTime=System.currentTimeMillis(); 
 	        
 	        PhoneNumberValidator validator = new PhoneNumberValidator();
 	        Set<PhoneNumber> set = validator.getValidaPhoneNumber(phoneNumbers);
 	        for (PhoneNumber phoneNum : set) {
 	        	long nationalNumber = phoneNum.getNationalNumber();
 	            System.out.println(nationalNumber  + " " );
-	        }	        
+	        }
+	        
+	        long endTime=System.currentTimeMillis();
+
+	        System.out.println("Run time:  "+(endTime-startTime)+"ms"); 
+	        
+	        System.out.println("\n2nd round Validated Cell Phone Numbers:");
+	        startTime=System.currentTimeMillis();  
+	        
+	        set = validator.getValidaPhoneNumber(phoneNumbers2);
+	        for (PhoneNumber phoneNum : set) {
+	        	long nationalNumber = phoneNum.getNationalNumber();
+	            System.out.println(nationalNumber  + " " );
+	        }
+	        
+	        endTime=System.currentTimeMillis();  
+	        System.out.println("Run time:  "+(endTime-startTime)+"ms"); 
+	        
 	    }	
 }
--- NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/LRUCache.java
+++ NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/LRUCache.java
@@ -1,5 +1,8 @@
 /*
  * Design and implement a data structure for Least Recently Used (LRU) cache. 
+ * The LRU cache is a hash table of keys and double linked nodes. The hash table makes the time of get() to be O(1). 
+ * The list of double linked nodes make the nodes adding/removal operations O(1).
+ * 
  * It support the following operations: get and set.
  * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
  * set(key, value) - Set or insert the value if the key is not already present.
