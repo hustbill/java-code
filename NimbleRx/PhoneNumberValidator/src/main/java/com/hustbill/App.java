e7f962667d56a539f601a50abcddf172dd32a90e
Hua Zhang <hua.zhang@wsu.edu>
2016-05-06 00:04:10 -0700

    Add LRUCache for cacheing the phone number which determined by twilio

--- NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/App.java
+++ NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/App.java
@@ -15,8 +15,8 @@ public class App {
 	public static void main(String[] args) throws TwilioRestException {
 		
 	        List<String> phoneNumbers = new ArrayList<String>();
-	        phoneNumbers.add("360-553-2393");
-	        phoneNumbers.add("408.441.8300");
+	        phoneNumbers.add("360-553-2393");  // MOBILE
+	        phoneNumbers.add("408.441.8300");  // LANDLINE
 	   
 	        //Invalid phone numbers
 	        phoneNumbers.add("(123) 456 7890");
@@ -24,14 +24,13 @@ public class App {
 	        phoneNumbers.add("12-12-111");
 	        phoneNumbers.add("280 456 7890");
 
-	        System.out.println("Validated Phone Numbers:");
+	        System.out.println("Validated Cell Phone Numbers:");
 	        
 	        PhoneNumberValidator validator = new PhoneNumberValidator();
 	        Set<PhoneNumber> set = validator.getValidaPhoneNumber(phoneNumbers);
 	        for (PhoneNumber phoneNum : set) {
 	        	long nationalNumber = phoneNum.getNationalNumber();
-	            System.out.print(nationalNumber  + " " );
-	            System.out.print(validator.twilioLookup(String.valueOf(nationalNumber)) + "\n");
+	            System.out.println(nationalNumber  + " " );
 	        }	        
 	    }	
 }
--- NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/PhoneNumberValidator.java
+++ NimbleRx/PhoneNumberValidator/src/main/java/com/hustbill/PhoneNumberValidator.java
@@ -1,12 +1,15 @@
+/*
+ * Phone Number Validator
+ * 
+ * Steps: 
+ * 		1.Determine if the phone number is a valid phone number using Google's libphonenumber 
+ * 		2.Determine if the phone number is a cell phone by Call Twilio Lookup API 
+ * 
+ * Date: 2016/05/05
+ * 
+ */
 package com.hustbill;
 
-/*
-Desc: Phone Number Validator
-Date: 2016/05/04
-Complie & Run:
-javac -cp "./lib/libphonenumber-7.3.2.jar" PhoneNumberTest.java
-java -cp  ".:./lib/libphonenumber-7.3.2.jar"  PhoneNumberTest
-*/
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,72 +20,88 @@ import com.twilio.sdk.LookupsClient;
 import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.resource.instance.lookups.PhoneNumber.Type;
 
-
 public class PhoneNumberValidator {
-    // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "AC180c23689eab0b992235e6d14a1efd91";
-    public static final String AUTH_TOKEN = "7b290425b3a0d209bcbd74be314574ea";
-      
+	// Find your Account Sid and Token at twilio.com/user/account
+	public static final String ACCOUNT_SID = "AC180c23689eab0b992235e6d14a1efd91";
+	public static final String AUTH_TOKEN = "7b290425b3a0d209bcbd74be314574ea";
 
-    /*
-     * using twilioLookup to determine whether a phone is a landline or mobile
-	    Twilio Lookup is a simple REST API with a ton of utility. 
-	    Twilio https://www.twilio.com/lookup 
-	    determine whether a phone is a landline or can receive text messages, 
-	    and even discover information about the carrier associated with that phone number.
-     */
-    public static String twilioLookup(String strNum) throws TwilioRestException {
-        LookupsClient client = new LookupsClient(ACCOUNT_SID, AUTH_TOKEN);
+	/*
+	 * Get the type of a phone : Landline or Mobile by Twilio Lookup API. 
+	 * 
+	 * Twilio Lookup is a simple REST API with a ton of utility. Twilio
+	 * https://www.twilio.com/lookup determine whether a phone is a landline or
+	 * can receive text messages, and even discover information about the
+	 * carrier associated with that phone number.
+	 */
+	public static String twilioLookup(PhoneNumber number) throws TwilioRestException {
+		String strNum = String.valueOf(number.getNationalNumber());
 
-        com.twilio.sdk.resource.instance.lookups.PhoneNumber phoneNumber =
-        		client.getPhoneNumber(strNum, true);
-        
-        return phoneNumber.getType().toString();
-      }
-      
+		LookupsClient client = new LookupsClient(ACCOUNT_SID, AUTH_TOKEN);
+		com.twilio.sdk.resource.instance.lookups.PhoneNumber phoneNumber = client.getPhoneNumber(strNum, true);
 
-    /*
-     * using libphonenumber
-     * Google's common Java, C++ and JavaScript library for parsing, formatting, 
-     *    and validating international phone numbers.
-     * 
-     */
-    public static Set<PhoneNumber> getValidaPhoneNumber(final Collection<String> phoneNumbers) {
-        Set<PhoneNumber> result = new HashSet<PhoneNumber>();
-        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+		return phoneNumber.getType().toString();
+	}
 
-        for(String usaNumberStr : phoneNumbers) {
-            try {
-                PhoneNumber usaNumberProto = phoneUtil.parse(usaNumberStr, "US");
-                boolean isValid = phoneUtil.isValidNumber(usaNumberProto);
-                if (isValid) {
-                    result.add(usaNumberProto);
-                }
-            } catch (Exception e) {
-                System.err.println("NumberParseException was thrown: " + e.toString());
-            }
-        }
-        return result;
-    }
+	/* Determine if the phone number is a valid phone number using Google's libphonenumber
+	 * 
+	 * Google's common Java, C++ and JavaScript library for
+	 * parsing, formatting, and validating international phone numbers.
+	 */
+	public static Set<PhoneNumber> getValidaPhoneNumber(final Collection<String> phoneNumbers) {
+		Set<PhoneNumber> result = new HashSet<PhoneNumber>();
+		PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+		LRUCache cache = new LRUCache(1000);
 
-       
+		for (String usaNumberStr : phoneNumbers) {
+			try {
+				PhoneNumber usaNumberProto = phoneUtil.parse(usaNumberStr, "US");
+				boolean isValid = phoneUtil.isValidNumber(usaNumberProto);
+			
+				if (isValid) {
+					long key =  usaNumberProto.getNationalNumber();
+					if (cache.get(key) != -1) {
+						if (cache.get(key) == 1) {  // 1 - MOBILE
+							result.add(usaNumberProto);
+						}
+					} else {
+						if (twilioLookup(usaNumberProto) == "MOBILE") {
+							result.add(usaNumberProto);
+							cache.set(key, 1);
+						} else {
+							cache.set(key, 0);  // 0 - LANDLINE
+						}
+					}
+				}				
+			} catch (Exception e) {
+				System.err.println("NumberParseException was thrown: " + e.toString());
+			}
+		}
+		return result;
+	}
+
 }
 
 /*
-    Ref: 
-    1. https://github.com/googlei18n/libphonenumber
-        Google's common Java, C++ and JavaScript library for parsing, formatting, and validating international phone numbers.
-    
-    2. https://www.twilio.com/docs/quickstart/php/lookups
-    3. https://www.twilio.com/docs/api/lookups
-    4. https://www.twilio.com/blog/2016/02/how-to-verify-phone-numbers-in-python-with-the-twilio-lookup-api.html
-
-curl -XGET https://lookups.twilio.com/v1/PhoneNumbers/+13605532393/?Type=caller-name \
--u 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
-
- curl -XGET https://lookups.twilio.com/v1/PhoneNumbers/+15108675309/?Type=carrier&Type=caller-name \
--u 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
-
- curl -XGET https://lookups.twilio.com/v1/PhoneNumbers/+13605532393/?Type=carrier&Type=caller-name \
--u 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
-*/
+ * Ref: 1. https://github.com/googlei18n/libphonenumber Google's common Java,
+ * C++ and JavaScript library for parsing, formatting, and validating
+ * international phone numbers.
+ * 
+ * 2. https://www.twilio.com/docs/quickstart/php/lookups 3.
+ * https://www.twilio.com/docs/api/lookups 4.
+ * https://www.twilio.com/blog/2016/02/how-to-verify-phone-numbers-in-python-
+ * with-the-twilio-lookup-api.html
+ * 
+ * curl -XGET
+ * https://lookups.twilio.com/v1/PhoneNumbers/+13605532393/?Type=caller-name \
+ * -u 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
+ * 
+ * curl -XGET
+ * https://lookups.twilio.com/v1/PhoneNumbers/+15108675309/?Type=carrier&Type=
+ * caller-name \ -u
+ * 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
+ * 
+ * curl -XGET
+ * https://lookups.twilio.com/v1/PhoneNumbers/+13605532393/?Type=carrier&Type=
+ * caller-name \ -u
+ * 'AC180c23689eab0b992235e6d14a1efd91:7b290425b3a0d209bcbd74be314574ea'
+ */
