48672290eded534fce1c220fcbc7911c8fa1cc7d
Hua Zhang <hua.zhang@wsu.edu>
2016-05-06 14:03:29 -0700

    filter the log

--- logStat/FileWordCount.java
+++ logStat/FileWordCount.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * Created by IntelliJ IDEA.
- * User: FLY
- * Date: 11-9-13
- * Time: 下午3:59
- * To change this template use File | Settings | File Templates.
- */
+* Created by IntelliJ IDEA.
+* User: FLY
+* Date: 11-9-13
+* Time: 下午3:59
+* To change this template use File | Settings | File Templates.
+*/
 public class FileWordCount {
     public static void main(String[] args) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("D:\\test.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("./data"));
             String s;
             StringBuffer sb = new StringBuffer();
             while ((s = br.readLine()) != null) {
@@ -40,28 +40,16 @@ public class FileWordCount {
             for (String key : map.keySet()) {
                 set.add(new WordEntity(key,map.get(key)));
             }
-            // 自己拼接字符串，输出我们想要的字符串格式
-            System.out.println("输出形式一：");
+            System.out.println("Print stat result:");
             for (Iterator<WordEntity> it = set.iterator(); it.hasNext(); ) {
                 WordEntity w = it.next();
-                System.out.println("单词:" + w.getKey() + " 出现的次数为： " + w.getCount());
-            }
-            // 直接打印 WordEntity 对象，实现我们想要的输出效果，只需在WordEntity类中重写toString()方法
-            System.out.println("输出形式二：");
-            for (Iterator<WordEntity> it = set.iterator(); it.hasNext(); ) {
-                WordEntity w = it.next();
-                System.out.println(w);
-            }
-            // 我们可以控制只输出前三名来
-            System.out.println("输出形式三：");
-            int count = 1;
-            for (Iterator<WordEntity> it = set.iterator(); it.hasNext(); ) {
-                WordEntity w = it.next();
-                System.out.println("第" + count + "名为单词:" + w.getKey() + " 出现的次数为： "
-                        + w.getCount());
-                if (count == 3)// 当输出3个后跳出循环
-                    break;
-                count++;
+
+                if (w.getKey().equals("INFO:")) {
+                    System.out.println("INFO Count :" +  w.getCount());
+                }
+                if (w.getKey().equals("WARN:")) {
+                    System.out.println("WARN Count :" +  w.getCount());
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("文件未找到~！");
