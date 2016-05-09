3eaf7ab69359eb089e590ad1452b9f02eeb6a439
Hua Zhang <hua.zhang@wsu.edu>
2016-05-08 23:26:24 -0700

    HammingWeight_191.java

---
+++ src/v201605/HammingWeight_191.java
@@ -0,0 +1,22 @@
+/*
+191. Number of 1 Bits   My Submissions QuestionEditorial Solution
+Total Accepted: 90018 Total Submissions: 239682 Difficulty: Easy
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+
+Hide Company Tags Microsoft Apple
+Hide Tags Bit Manipulation
+Hide Similar Problems (E) Reverse Bits (E) Power of Two (M) Counting Bits
+
+*/
+
+public class HammingWeight_191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+    }
+}
