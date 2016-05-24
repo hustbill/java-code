2d377f0835ebef95b44ae77d290caa3d63bb70fe
Hua Zhang <hua.zhang@wsu.edu>
2016-05-24 09:14:46 -0700

    http://www.javatpoint.com/static-keyword-in-java

---
+++ coreJava/Student8.java
@@ -0,0 +1,25 @@
+//Example of static variable
+// http://www.javatpoint.com/static-keyword-in-java
+// Date: May 24, 2016
+
+class Student8{  
+    int rollno;  
+    String name;  
+    static String college ="ENCSs";  
+     
+    Student8(int r,String n){  
+        rollno = r;  
+        name = n;  
+    }  
+    void display (){
+        System.out.println(rollno+" "+name+" "+college);
+    }  
+  
+    public static void main(String args[]){  
+        Student8 s1 = new Student8(111,"Karan");  
+        Student8 s2 = new Student8(222,"Aryan");  
+   
+        s1.display();  
+        s2.display();  
+    }  
+}  
