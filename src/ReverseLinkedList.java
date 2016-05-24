36fa7a201280f22013fd11a150b074b82ad063b4
Hua Zhang <hua.zhang@wsu.edu>
2016-05-23 22:05:30 -0700

    src/ReverseLinkedList.java

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+public class ReverseLinkedList {
+
+	public ReverseLinkedList() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		list.add(7);
+		
+		System.out.println(reverseLinkedList(list));
+
+	}
+	
+	public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> list) {
+		LinkedList<Integer> result = new LinkedList<>();
+		
+		if (list  == null) return list;
+		
+		// 
+		
+		
+		
+		
+		
+	}
+	
+	public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> list) {
+		LinkedList<Integer> result = new LinkedList<>();
+		
+		if (list  == null) return list;
+		
+		// 
+		
+		
+		
+		
+		
+	}
+
+}
