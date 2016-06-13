d2e75dd86ea6b93af4dcd882f53e17b9962d6f03
Hua Zhang <hua.zhang@wsu.edu>
2016-06-13 08:52:47 -0700

    update isPowerOfTwo, three, four, test cases

--- src/v201604/IsPowerOfTwo_231Test.java
+++ src/v201604/IsPowerOfTwo_231Test.java
@@ -1,17 +1,14 @@
 package v201604;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class IsPowerOfTwo_231Test {
-	@BeforeTest
-	public void beforeTest() {
-	}
 
-	@AfterTest
-	public void afterTest() {
+	@Before
+	public void setUp() throws Exception {
 	}
 
 	@Test
@@ -21,14 +18,14 @@ public class IsPowerOfTwo_231Test {
 		int[] nums = { 128, 1 };
 		for (int n : nums) {
 			boolean result = obj.isPowerOfTwo(n);
-			Assert.assertEquals(result, true);
+			assertEquals(result, true);
 		}
 
 		int[] failedNums = { 1280, -16 };
 
 		for (int n : failedNums) {
 			boolean result = obj.isPowerOfTwo(n);
-			Assert.assertEquals(result, false);
+			assertEquals(result, false);
 		}
 	}
 
@@ -38,14 +35,14 @@ public class IsPowerOfTwo_231Test {
 		int[] nums = { 9, 27 };
 		for (int n : nums) {
 			boolean result = obj.isPowerOfThree(n);
-			Assert.assertEquals(result, true);
+			assertEquals(result, true);
 		}
 
 		int[] failedNums = { 53, -16 };
 
 		for (int n : failedNums) {
 			boolean result = obj.isPowerOfThree(n);
-			Assert.assertEquals(result, false);
+			assertEquals(result, false);
 		}
 	}
 	
@@ -55,14 +52,15 @@ public class IsPowerOfTwo_231Test {
 		int[] nums = { 16, 1024 };
 		for (int n : nums) {
 			boolean result = obj.isPowerOfFour(n);
-			Assert.assertEquals(result, true);
+			assertEquals(result, true);
 		}
 
 		int[] failedNums = { 2048, 97, -16};
 
 		for (int n : failedNums) {
 			boolean result = obj.isPowerOfFour(n);
-			Assert.assertEquals(result, false);
+			assertEquals(result, false);
 		}
 	}
+
 }
