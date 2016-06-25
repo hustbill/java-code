78c31620d1d01e0a018b33c85348fd7bde9409ef
Hua Zhang <hua.zhang@wsu.edu>
2016-06-25 11:36:23 -0700

    check the value of row1 col1 , row2 col2

--- src/dp/NumMatrix.java
+++ src/dp/NumMatrix.java
@@ -1,3 +1,33 @@
+/*
+ * 304 - Range Sum Query 2D - Immutable
+
+Total Accepted: **14771** Total Submissions: **66244** Difficulty: **Medium**
+
+Given a 2D matrix *matrix*, find the sum of the elements inside the rectangle defined by its upper left corner (*row*1, *col*1) and lower right corner (*row*2, *col*2).
+
+The above rectangle (with the red border) is defined by (row1, col1) = **(2, 1)** and (row2, col2) = **(4, 3)**, which contains sum = **8**.
+**Example:**
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+
+**Note:**
+You may assume that the matrix does not change.
+There are many calls to *sumRegion* function.
+You may assume that *row*1 ≤ *row*2 and *col*1 ≤ *col*2.
+
+Hide Tags Dynamic Programming
+Hide Similar Problems (E) Range Sum Query - Immutable (H) Range Sum Query 2D - Mutable
+
+ */
 package dp;
 
 public class NumMatrix {
@@ -23,7 +53,15 @@ public class NumMatrix {
 	}
 
 	public int sumRegion(int row1, int col1, int row2, int col2) {
-		return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+		// if assurm row1 <= row2 and col1 <= col2  
+	       //return dp[row2 + 1][col2 + 1 ] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+	        int iMin = Math.min(row1, row2);
+	        int iMax = Math.max(row1, row2);
+
+	        int jMin = Math.min(col1, col2);
+	        int jMax = Math.max(col1, col2);
+
+	        return dp[iMax + 1][jMax + 1] - dp[iMax + 1][jMin] - dp[iMin][jMax + 1] + dp[iMin][jMin];  
 	}
 
 	public static void main(String[] args) {
