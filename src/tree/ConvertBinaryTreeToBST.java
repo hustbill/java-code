1731dbc6310373c8268182bf046f54f0e19c4f2f
Hua Zhang <hua.zhang@wsu.edu>
2016-06-26 21:20:01 -0700

    MS Azure OTS 6/26/16  Convert Binary tree to Binary search tree

---
+++ src/tree/ConvertBinaryTreeToBST.java
@@ -0,0 +1,60 @@
+package tree;
+
+import java.util.*;
+
+public class ConvertBinaryTreeToBST {
+
+	// Time complexity O(n), Space Complexity O(n)
+	public static TreeNode convertTree(TreeNode root) {
+		List<Integer> list = inorderTraversal(root);
+		if (list.isEmpty())
+			return null;
+		Collections.sort(list);
+		return buildBST(list);
+	}
+
+	// Time complexity O(n), Space Complexity O(n)
+	public static TreeNode buildBST(List<Integer> nums) {
+		if (nums == null || nums.isEmpty())
+			return null;
+		return buildHelper(nums, 0, nums.size() - 1);
+	}
+
+	public static TreeNode buildHelper(List<Integer> nums, int begin, int end) {
+		if (begin <= end) {
+			int mid = begin + (end - begin) / 2;
+			TreeNode root = new TreeNode(nums.get(mid));
+			root.left = buildHelper(nums, begin, mid - 1);
+			root.right = buildHelper(nums, mid + 1, end);
+			return root;
+		}
+		return null;
+	}
+
+	// Time complexity O(n), Space Complexity O(n)
+	public static List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		if (root == null)
+			return res;
+		inorderHelper(root, res);
+		return res;
+	}
+
+	public static void inorderHelper(TreeNode root, List<Integer> res) {
+		if (root == null)
+			return;
+
+		inorderHelper(root.left, res);
+		res.add(root.val);
+		inorderHelper(root.right, res);
+	}
+}
+
+//class TreeNode {
+//	int val;
+//	TreeNode left, right;
+//
+//	public TreeNode(int val) {
+//		this.val = val;
+//	}
+//}
