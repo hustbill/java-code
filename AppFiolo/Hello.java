36283958b73b99aea164f05a3c0016cae1a751d3
Hua Zhang <hua.zhang@wsu.edu>
2016-06-28 20:17:02 -0700

    AppFiolo coding questions

--- AppFiolo/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello 
-{ 
-    public static void main(String args[]) 
-{ 
-    System.out.println("Hello Java 2"); //中文注释
-} 
-}
--- Macys/Convert2Tuple.scala
+++
@@ -1,5 +0,0 @@
-scala> val lists = List(((1,52),7), ((1,82),6))
-lists: List[((Int, Int), Int)] = List(((1,52),7), ((1,82),6))
-
-scala> lists map { case ((a,b),c ) => (a,b,c) }
-res7: List[(Int, Int, Int)] = List((1,52,7), (1,82,6))
