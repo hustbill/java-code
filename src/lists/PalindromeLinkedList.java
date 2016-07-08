52414af4448182fa54561f72fadce1949cb03c42
Hua Zhang <hua.zhang@wsu.edu>
2016-07-07 21:34:45 -0700

    Palindrome LinkedList

--- src/lists/PalindromeLinkedList.java
+++ src/lists/PalindromeLinkedList.java
@@ -1,9 +1,42 @@
 /*
- * 
+ * 234. Palindrome Linked List  QuestionEditorial Solution  My Submissions
+Total Accepted: 55152
+Total Submissions: 188771
+Difficulty: Easy
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+Hide Company Tags Amazon Facebook
+Hide Tags Linked List Two Pointers
+Hide Similar Problems (E) Palindrome Number (E) Valid Palindrome (E) Reverse Linked List
+
  */
 package lists;
 
 public class PalindromeLinkedList {
+    /*
+   This can be solved by reversing the 2nd half and compare the two halves. Let's start with an example [1, 1, 2, 1].
+   
+   In the beginning, set two pointers fast and slow starting at the head.
+   
+   1 -> 1 -> 2 -> 1 -> null 
+   sf
+   (1) Move: fast pointer goes to the end, and slow goes to the middle.
+   
+   1 -> 1 -> 2 -> 1 -> null 
+             s          f
+   (2) Reverse: the right half is reversed, and slow pointer becomes the 2nd head.
+   
+   1 -> 1    null <- 2 <- 1           
+   h                      s
+   (3) Compare: run the two pointers head and slow together and compare.
+   
+   1 -> 1    null <- 2 <- 1             
+        h            s
+   In this end, check if slow == null. For this example, since slow != null, return false.
+   */
 
 	public boolean isPalindrome(ListNode head) {
 		ListNode fast = head;
