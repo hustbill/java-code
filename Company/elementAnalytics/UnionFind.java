9656358a0f11f4cec1f86e27942d6459d02d530c
Hua Zhang <hua.zhang@wsu.edu>
2016-07-12 21:15:19 -0700

    My union find solution - doesn't fininshed.

--- Company/elementAnalytics/UnionFind.java
+++ Company/elementAnalytics/UnionFind.java
@@ -1,22 +1,33 @@
 package elementAnalytics;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 class Node {
 	int name;
 	Node[] neibors;
 }
 
 public class UnionFind {
-	
-	public UnionFind() {
-		
-	}
-	
 	int[] parent ;
 	int[] size;
-	
-	public int findRoot(int a, int b) {
-		// find 
-	}
+
+    Map<Integer, Integer> map=new HashMap<>();
+    public UnionFind(Set<Integer> set){
+        for(int n : set) map.put(n, n);
+    }
+    public int find(int node){
+        int p=map.get(node);
+        while(p!=map.get(p)){
+            p=map.get(p);
+        }
+        return p;
+    }
+    public void union(int p1, int p2){
+        map.put(p1, p2);
+    
+    } 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
