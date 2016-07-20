558dbe8b39c1568d2d439b20251582706f4d8a77
Hua Zhang <hua.zhang@wsu.edu>
2016-07-20 10:47:36 -0700

    Lambda expression, stream, reduct, aggr operation

--- src/java8/LargeNumberStream.java
+++ src/java8/LargeNumberStream.java
@@ -24,6 +24,7 @@ public class LargeNumberStream {
     // https://discuss.leetcode.com/topic/7235/my-3-lines-code-in-java-and-python
     public String largestNumber(int[] num) {  // Thanks for Java 8, it makes code beautiful.
         String[] array = Arrays.stream(num).mapToObj(String::valueOf).toArray(String[]::new);
+        // https://docs.oracle.com/javase/tutorial/collections/streams/reduction.html
         Arrays.sort(array, (String s1, String s2) -> (s2 + s1).compareTo(s1 + s2));
         return Arrays.stream(array).reduce((x, y) -> x.equals("0") ? y : x + y).get();
     }
