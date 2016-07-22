a4442300b4f54b501c1f78212d9446bd2e33222e
Hua Zhang <hua.zhang@wsu.edu>
2016-07-21 23:57:33 -0700

    Maximum Sum Subarray

---
+++ src/dp/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class MaximumSubarray {
+	  public int maxSubArray1(int[] nums) {
+	        // Solution1: 
+	        // Refer: https://leetcode.com/discuss/15805/accepted-o-n-solution-in-java
+	        // update 7/21/16 
+	        int[] A= nums;
+	        int maxSoFar = A[0], maxEndingHere=A[0];
+	        for (int i=1; i<A.length; ++i) {
+	            maxEndingHere = Math.max(maxEndingHere + A[i], A[i]);
+	            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+	        }
+	        return maxSoFar;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
