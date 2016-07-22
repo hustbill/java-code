50767556310ab3b5e035c8ba499b5c55c851c6da
Hua Zhang <hua.zhang@wsu.edu>
2016-07-21 23:59:50 -0700

    update

--- src/dp/MaximumSubarray.java
+++ src/dp/MaximumSubarray.java
@@ -5,6 +5,8 @@ public class MaximumSubarray {
 	        // Solution1: 
 	        // Refer: https://leetcode.com/discuss/15805/accepted-o-n-solution-in-java
 	        // update 7/21/16 
+		  // f(k) =  Maximum sum of subarray ending at index k.
+		  // f(k) = max(f(k-1) + A[k], A[k]);
 	        int[] A= nums;
 	        int maxSoFar = A[0], maxEndingHere=A[0];
 	        for (int i=1; i<A.length; ++i) {
