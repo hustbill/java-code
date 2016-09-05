4d73e7668c7e9ec6fbc5e6e805efd504df447e67
Hua Zhang <hua.zhang@wsu.edu>
2016-09-05 10:38:24 -0700

    81. Search In Rotated Array II ( With Duplicates)

--- src/array/SearchInRoatedArrayLinkedIn.java
+++ src/array/SearchInRoatedArrayLinkedIn.java
@@ -2,6 +2,9 @@ package array;
 /*
 3/29/16 LinkedIn Phone interview 
 Kevin Lu
+9/5
+刷题不熟练
+
 
 */
 
@@ -24,10 +27,12 @@ Search for 7
 public class SearchInRoatedArrayLinkedIn {
 	public static void main(String[] args) {
 		// int[] array = {1, 2, 1, 1, 1};
-		// int[] array = {1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1};
+		 int[] arrayWithDuplicates = {1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1};
 		int[] array = { 6, 7, 1, 2, 3, 4, 6 };
-		// System.out.print(findTarget(array, 2));
-		System.out.print(search(array, 2));
+	
+		System.out.println(search(array, 2)); // 3
+		System.out.println(searchInRotatedArrayWithDuplicates(array, 2));  // true
+		System.out.println(searchInRoatedArrayWithDuplicates(array, 2)); //3
 	}
 
 	// this solution is able to solve {1, 1, 2, 1, 1, 1, 1, 1, 1, 1} 2 => 2
@@ -78,7 +83,8 @@ public class SearchInRoatedArrayLinkedIn {
 	// date : 2016.04.03 failed at duplicate items in the roated sorted array.
 	// date : 2016.06.26 33. Search in Rotated Sorted Array 本来就不包含 duplicate
 	// items.
-	// 	 test case 1. [6,7,1,2,3,4,5] 4 =>5 2. [3,1] 1 => 1
+	// date : 2016.09.05  leetcode 81. search in roated sorted array II 包含duplicates
+	// test case 1. [6,7,1,2,3,4,5] 4 =>5 2. [3,1] 1 => 1
 	public static int search(int[] nums, int target) {
 		/*
 		 * 先用中点分成两个 parts，判断target 是否在这两段中（还有判断是否是sorted part）。
@@ -99,7 +105,7 @@ public class SearchInRoatedArrayLinkedIn {
 
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
-			int midVal = nums[mid] ;
+			int midVal = nums[mid];
 			if (midVal == target) {
 				return mid;
 			}
@@ -122,4 +128,95 @@ public class SearchInRoatedArrayLinkedIn {
 		}
 		return -1;
 	}
+
+	/*  81. Search in Rotated Sorted Array II
+	 * Follow up for "Search in Rotated Sorted Array": What if duplicates are
+	 * allowed?
+	 * 
+	 * Would this affect the run-time complexity? How and why?
+	 */
+	public static boolean searchInRotatedArrayWithDuplicates(int[] nums, int target) {
+		int start = 0, end = nums.length - 1;
+
+		// check each num so we will check start == end
+		// We always get a sorted part and a half part
+		// we can check sorted part to decide where to go next
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] == target)
+				return true;
+
+			// if left part is sorted
+			if (nums[start] < nums[mid]) {
+				if (target < nums[start] || target > nums[mid]) {
+					// target is in rotated part
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			} else if (nums[start] > nums[mid]) {
+				// right part is rotated
+
+				// target is in rotated part
+				if (target < nums[mid] || target > nums[end]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			} else {
+				// duplicates, we know nums[mid] != target, so nums[start] !=
+				// target
+				// based on current information, we can only move left pointer
+				// to skip one cell
+				// thus in the worest case, we would have target: 2, and array
+				// like 11111111, then
+				// the running time would be O(n)
+				start++;
+			}
+		}
+		return false;
+	}
+	
+	public static int searchInRoatedArrayWithDuplicates(int[] nums, int target) {
+		int start = 0, end = nums.length - 1;
+
+		// check each num so we will check start == end
+		// We always get a sorted part and a half part
+		// we can check sorted part to decide where to go next
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] == target)
+				return mid;
+
+			// if left part is sorted
+			if (nums[start] < nums[mid]) {
+				if (target < nums[start] || target > nums[mid]) {
+					// target is in rotated part
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			} else if (nums[start] > nums[mid]) {
+				// right part is rotated
+
+				// target is in rotated part
+				if (target < nums[mid] || target > nums[end]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			} else {
+				// duplicates, we know nums[mid] != target, so nums[start] !=
+				// target
+				// based on current information, we can only move left pointer
+				// to skip one cell
+				// thus in the worest case, we would have target: 2, and array
+				// like 11111111, then
+				// the running time would be O(n)
+				start++;
+			}
+		}
+		return start;
+	}
+
 }
