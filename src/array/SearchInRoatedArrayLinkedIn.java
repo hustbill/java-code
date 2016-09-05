60c8760bb384f13146f64d01d063863d075381ed
Hua Zhang <hua.zhang@wsu.edu>
2016-09-05 11:28:48 -0700

    153. Find Minimum in Rotated Sorted Array

--- src/array/SearchInRoatedArrayLinkedIn.java
+++ src/array/SearchInRoatedArrayLinkedIn.java
@@ -27,12 +27,42 @@ Search for 7
 public class SearchInRoatedArrayLinkedIn {
 	public static void main(String[] args) {
 		// int[] array = {1, 2, 1, 1, 1};
-		 int[] arrayWithDuplicates = {1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1};
+		int[] arrayWithDuplicates = { 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1 };
 		int[] array = { 6, 7, 1, 2, 3, 4, 6 };
-	
+
 		System.out.println(search(array, 2)); // 3
-		System.out.println(searchInRotatedArrayWithDuplicates(array, 2));  // true
-		System.out.println(findInRotatedArrayWithDuplicates(array, 2)); //3
+		System.out.println(searchInRotatedArrayWithDuplicates(array, 2)); // true
+		System.out.println(findInRotatedArrayWithDuplicates(array, 2)); // 3
+	}
+
+	/*
+	 * 153. Find Minimum in Rotated Sorted Array
+	 * Difficulty: Medium 
+	 * Suppose a sorted array is rotated at some pivot
+	 * unknown to you beforehand.
+	 * 
+	 * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+	 * 
+	 * Find the minimum element.
+	 */
+	public int findMin(int[] nums) {
+		// find the index of the smallest value using binary search.
+		// Loop will terminate since mid < hi, and lo or hi will shrink by at
+		// least 1.
+		// Proof by contradiction that mid < hi: if mid==hi, then lo==hi and
+		// loop would have been terminated.
+		int start = 0, end = nums.length - 1;
+
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+			if (nums[end] < nums[mid]) {
+				start = mid + 1; // 转折点在mid 的右侧
+			} else {
+				end = mid;
+			}
+		}
+		return nums[start];
+		// start == end is the smallest value and also the number of places
 	}
 
 	// this solution is able to solve {1, 1, 2, 1, 1, 1, 1, 1, 1, 1} 2 => 2
@@ -80,10 +110,12 @@ public class SearchInRoatedArrayLinkedIn {
 
 	}
 
+	// 33. Search in Rotated Sorted Array
 	// date : 2016.04.03 failed at duplicate items in the roated sorted array.
 	// date : 2016.06.26 33. Search in Rotated Sorted Array 本来就不包含 duplicate
 	// items.
-	// date : 2016.09.05  leetcode 81. search in roated sorted array II 包含duplicates
+	// date : 2016.09.05 leetcode 81. search in roated sorted array II
+	// 包含duplicates
 	// test case 1. [6,7,1,2,3,4,5] 4 =>5 2. [3,1] 1 => 1
 	public static int search(int[] nums, int target) {
 		/*
@@ -129,9 +161,9 @@ public class SearchInRoatedArrayLinkedIn {
 		return -1;
 	}
 
-	/*  81. Search in Rotated Sorted Array II
-	 * Follow up for "Search in Rotated Sorted Array": What if duplicates are
-	 * allowed?
+	/*
+	 * 81. Search in Rotated Sorted Array II Follow up for
+	 * "Search in Rotated Sorted Array": What if duplicates are allowed?
 	 * 
 	 * Would this affect the run-time complexity? How and why?
 	 */
@@ -148,20 +180,17 @@ public class SearchInRoatedArrayLinkedIn {
 
 			// if left part is sorted
 			if (nums[start] < nums[mid]) {
-				if (target < nums[start] || target > nums[mid]) {
-					// target is in rotated part
-					start = mid + 1;
-				} else {
+				if (target >= nums[start] && target < nums[mid]) {
 					end = mid - 1;
+				} else {
+					start = mid + 1; // target is in rotated part
 				}
-			} else if (nums[start] > nums[mid]) {
-				// right part is rotated
-
+			} else if (nums[start] > nums[mid]) { // right part is rotated
 				// target is in rotated part
-				if (target < nums[mid] || target > nums[end]) {
-					end = mid - 1;
-				} else {
+				if (target >= nums[mid] && target <= nums[end]) {
 					start = mid + 1;
+				} else {
+					end = mid - 1;
 				}
 			} else {
 				// duplicates, we know nums[mid] != target, so nums[start] !=
@@ -176,9 +205,9 @@ public class SearchInRoatedArrayLinkedIn {
 		}
 		return false;
 	}
-	
+
 	// find the index of the target in the rotated array with duplicates
-	public static int findInRoatedArrayWithDuplicates(int[] nums, int target) {
+	public static int findInRotatedArrayWithDuplicates(int[] nums, int target) {
 		int start = 0, end = nums.length - 1;
 
 		// check each num so we will check start == end
