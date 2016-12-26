40f032b445f49e8731fac4795f22e3fcf63328ef
Hua Zhang <billyzhang2010@gmail.com>
2016-12-25 21:03:16 -0800

    189. Rotate Array
    Rotate an array of n elements to the right by k steps.
    
    For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].

---
+++ src/array/RotateArray.java
@@ -0,0 +1,34 @@
+ public class RotateArray {
+ 
+ /*
+    let a= [1,2,3,4,5,6,7]
+    k = 3.
+    
+    we have to first reverse the whole array by swapping first element with the last one and so on..
+    you will get[7,6,5,4,3,2,1]
+    
+    reverse the elements from 0 to k-1
+    reverse the elements 7,6,5
+    you will get [5,6,7,4,3,2,1]
+    
+    reverse the elements from k to n-1
+    reverse the elements 4,3,2,1
+    you will get[5,6,7,1,2,3,4]
+    */
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }    
+    }
+    
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+   }
