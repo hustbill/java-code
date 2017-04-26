bac605ed5de26305587e0c4ea15eff3aa82e9c99
Hua Zhang <hua.zhang1@huawei.com>
2017-04-25 20:52:03 -0700

    ziazag level order

--- src/bfs/LevelOrder.java
+++ src/bfs/LevelOrder.java
@@ -9,6 +9,9 @@ import java.util.*;
 /**
  * @author hzhang
  *
+ *102. level order traversal
+ *
+ *103. zigzag traversal
  */
 public class LevelOrder {
 
@@ -28,11 +31,16 @@ public class LevelOrder {
 		right.right = new TreeNode(7);
 		LevelOrder lr = new LevelOrder();
 		List<List<Integer>> res = lr.levelOrder(root);
+		List<List<Integer>> zigzagRes = lr.zigzagLevelOrder(root);
 		
 		
 		System.out.println(res);
+		System.out.println(zigzagRes);
 	}
 	
+	/*
+	 *  102 level order
+	 */
 	  public  List<List<Integer>> levelOrder(TreeNode root) {
 		  List<List<Integer>> res = new ArrayList<>();
 		  if (root == null) return res;
@@ -57,5 +65,33 @@ public class LevelOrder {
 		  
 		  return res;
 	   }
+	  
+	  /*
+	   *  103. zigzag level order 
+	   */
+	  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		  List<List<Integer>> res = new ArrayList<>();
+		  travel(root, res, 0);
+		  return res;
+	  }
+	  
+	  // dfs
+	  private void travel(TreeNode curr, List<List<Integer>> res, int level) {
+		  if (curr == null)  return;
+		  
+		  if (res.size() <= level) {
+			  List<Integer> newLevel = new LinkedList<>();
+			  res.add(newLevel);
+		  }
+		  
+		  List<Integer> collection = res.get(level);
+		  if (level % 2 == 0) collection.add(curr.val);
+		  else collection.add(0, curr.val);
+		  
+		  travel(curr.left, res, level + 1);
+		  travel(curr.right, res, level + 1);
+	  }
+	  
+	  
 
 }
