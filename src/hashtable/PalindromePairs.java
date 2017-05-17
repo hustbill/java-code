1d03fd31031e16911a55af6a5da4a8120399735f
Hua Zhang <billyzhang2010@gmail.com>
2017-05-17 10:34:09 -0700

    336.Palindrome Pairs

---
+++ src/hashtable/PalindromePairs.java
@@ -0,0 +1,25 @@
+/*
+336. Palindrome Pairs Add to List
+DescriptionHintsSubmissionsSolutions
+Total Accepted: 23522
+Total Submissions: 91603
+Difficulty: Hard
+Contributor: LeetCode
+Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+
+Example 1:
+Given words = ["bat", "tab", "cat"]
+Return [[0, 1], [1, 0]]
+The palindromes are ["battab", "tabbat"]
+Example 2:
+Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.
+
+Hide Company Tags Google Airbnb
+Hide Tags Hash Table String Trie
+Hide Similar Problems (M) Longest Palindromic Substring (H) Shortest Palindrome
+
+*/
