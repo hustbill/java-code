0f1038b45402a58ddca28d2df61559b015cb7e73
hua zhang <billyzhang2010@gmail.com>
2018-10-22 11:13:53 -0700

    describe the solution for Palindrom LinkedList

--- src/visa/practice/PalindromLinkedList.java
+++ src/visa/practice/PalindromLinkedList.java
@@ -6,25 +6,14 @@ public class PalindromLinkedList {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ListNode first = new ListNode(1);
-		first.next = new ListNode(2);
-		first.next.next = new ListNode(1);
-		boolean result = isPalindromLinkedList(first);
-		
-		System.out.println("the first linkedlist is " + result); // 1->2->1 true
-		
-		ListNode second = new ListNode(2);
-		second.next = first;
-		
-		result = isPalindromLinkedList(second); // 2->1->2-1  false
-		System.out.println("the second linkedlist is " + result);
-		
-		
-		
-	}
-	
+	/*
+	 * solution: 思路 
+	 * fast-slow pointer to get the left half and right half
+	 * reverse right half
+	 * compare left half and right half, if not equal, return false, else move to next 
+	 * and compare fast.next and slow.next
+	 * 
+	 */
 	public static boolean isPalindromLinkedList(ListNode head) {
 		ListNode fast = head;
 		ListNode slow = head;
@@ -58,6 +47,22 @@ public class PalindromLinkedList {
 			head = next;
 		}
 		return prev;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode first = new ListNode(1);
+		first.next = new ListNode(2);
+		first.next.next = new ListNode(1);
+		boolean result = isPalindromLinkedList(first);
+		
+		System.out.println("the first linkedlist is " + result); // 1->2->1 true
+		
+		ListNode second = new ListNode(2);
+		second.next = first;
+		
+		result = isPalindromLinkedList(second); // 2->1->2-1  false
+		System.out.println("the second linkedlist is " + result);
 	}
 
 }
