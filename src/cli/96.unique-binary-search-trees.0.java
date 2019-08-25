e100dc74179f3dddc72a12405ab93a5bd082a090
Hua Zhang <hua.zhang@wsu.edu>
2019-08-24 22:11:15 -0700

    dp solution

--- src/cli/96.unique-binary-search-trees.0.java
+++ src/cli/96.unique-binary-search-trees.0.java
@@ -32,7 +32,15 @@
  */
 class Solution {
     public int numTrees(int n) {
-        
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
     }
 
     public int numTrees1(int n) {
@@ -93,7 +101,7 @@ class Solution {
         return ans.size();
     }
     
-    public int numTrees(int n) {
+    public int numTrees2(int n) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         
          for (int i = 1; i <= n; i++) {
