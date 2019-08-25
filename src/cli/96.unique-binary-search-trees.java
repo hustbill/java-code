2c3f4028f2e40830a9a29f4aac52e0d28beccb8b
Hua Zhang <hua.zhang@wsu.edu>
2019-08-24 22:13:49 -0700

    96. dp solution explian

--- src/cli/96.unique-binary-search-trees.java
+++ src/cli/96.unique-binary-search-trees.java
@@ -31,6 +31,20 @@
  * 
  */
 class Solution {
+  	/** DP solution
+	 * https://leetcode.com/discuss/17674/dp-problem-10-lines-with-comments
+	 * Taking 1~n as root respectively:
+	 *      1 as root: # of trees = F(0) * F(n-1)  // F(0) == 1
+	 *      2 as root: # of trees = F(1) * F(n-2) 
+	 *      3 as root: # of trees = F(2) * F(n-3)
+	 *      ...
+	 *      n-1 as root: # of trees = F(n-2) * F(1)
+	 *      n as root:   # of trees = F(n-1) * F(0)
+	 *
+	 * So, the formulation is:
+	 *     F(n) = F(0) * F(n-1) + F(1) * F(n-2) + F(2) * F(n-3) + ... + F(n-2) * F(1) + F(n-1) * F(0)
+	 */
+
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
