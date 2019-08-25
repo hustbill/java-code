9a2599a877a06acb2ae155b7feb0299916842c98
Hua Zhang <hua.zhang@wsu.edu>
2019-08-25 13:00:51 -0700

    101.symmetric-tree.java

--- src/cli/101.symmetric-tree.java
+++ src/cli/101.symmetric-tree.java
@@ -53,6 +53,36 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+/*
+Time complexity : O(n)O(n). Because we traverse the entire input tree once, the total run time is O(n)O(n), where nn is the total number of nodes in the tree.
+
+Space complexity : The number of recursive calls is bound by the height of the tree. In the worst case, the tree is linear and the height is in O(n)O(n). Therefore, space complexity due to recursive calls on the stack is O(n)O(n) in the worst case. 
+*/
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+
+        if (t1 == null || t2 == null) return false;
+
+        return (t1.val == t2.val) 
+            && isMirror(t1.left, t2.right) 
+            && isMirror(t1.right, t2.left);
+    }
+
+
+    public boolean isSymmetric_wrong(TreeNode root) {
+        if (root == null) return true;
+
+        if (root.left == null && root.right != null) return false;
+        if (root.left != null && root.right == null) return false;
+    
+        if (root.left.val != root.right.val) {
+            return false;
+        }
+
+        return isSymmetric(root.left) && isSymmetric(root.right);
         
     }
 }
