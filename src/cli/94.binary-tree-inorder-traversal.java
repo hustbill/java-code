335bef22b81618e44f61e16846ff23262311b2eb
hua zhang <huazhang@visa.com>
2019-08-27 18:43:19 -0700

    94.binary-tree-inorder-traversal.java

---
+++ src/cli/94.binary-tree-inorder-traversal.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (58.02%)
+ * Total Accepted:    513K
+ * Total Submissions: 883.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode p = root;
+        
+        while (p != null || !stack.isEmpty()) {
+            while (p != null) {
+            // while (p != null || !stack.isEmpty()) { // 不要弄混了两个while loops
+                stack.push(p);
+                p = p.left;
+            }
+            p = stack.pop();
+            ans.add(p.val);
+            p = p.right;
+        }
+        return ans;
+    }
+}
