52c560f98b254f5384408b1351897290c0ad46ae
hua zhang <huazhang@visa.com>
2019-08-29 20:29:04 -0700

    101.symmetric-tree.java

--- src/cli/101.symmetric-tree.java
+++ src/cli/101.symmetric-tree.java
@@ -53,36 +53,17 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return isMirror(root, root);
+       return isMirror(root, root); 
     }
+    // Time: O(n), Space: O(n) 
+    // Recursive solution; another one is iterative using Stack
+    private boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null ||  node2 == null) return false;
 
-/*
-Time complexity : O(n)O(n). Because we traverse the entire input tree once, the total run time is O(n)O(n), where nn is the total number of nodes in the tree.
-
-Space complexity : The number of recursive calls is bound by the height of the tree. In the worst case, the tree is linear and the height is in O(n)O(n). Therefore, space complexity due to recursive calls on the stack is O(n)O(n) in the worst case. 
-*/
-    private boolean isMirror(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) return true;
-
-        if (t1 == null || t2 == null) return false;
-
-        return (t1.val == t2.val) 
-            && isMirror(t1.left, t2.right) 
-            && isMirror(t1.right, t2.left);
-    }
-
-
-    public boolean isSymmetric_wrong(TreeNode root) {
-        if (root == null) return true;
-
-        if (root.left == null && root.right != null) return false;
-        if (root.left != null && root.right == null) return false;
-    
-        if (root.left.val != root.right.val) {
+        if (node1.val != node2.val) {
             return false;
         }
-
-        return isSymmetric(root.left) && isSymmetric(root.right);
-        
+        return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
     }
 }
