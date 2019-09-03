40deaf3762af82d24d5b47a2cb19f94022c478ff
hua zhang <huazhang@visa.com>
2019-09-02 21:14:07 -0700

    107.binary-tree-level-order-traversal-ii.java

---
+++ src/cli/107.binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=107 lang=java
+ *
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (47.91%)
+ * Total Accepted:    245.5K
+ * Total Submissions: 510.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its
+ * nodes' values. (ie, from left to right, level by level from leaf to root).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its bottom-up level order traversal as:
+ * 
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int height = getHeight(root);
+        while (ans.size() < height) {
+            ans.add(new ArrayList<Integer>());
+        }
+        System.out.printf("ans.size() = %d", ans.size());
+        helper(root, ans, height - 1);
+        return ans;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        int lh = getHeight(root.left);
+        int rh = getHeight(root.right);
+        
+        return Math.max(lh, rh) + 1;
+   }
+
+
+    private void helper(TreeNode node, List<List<Integer>> ans, int height) {
+        if (node == null) return ;
+
+        ans.get(height).add(node.val); // 非常巧妙，需要记住！！
+        helper(node.left, ans, height - 1);
+        helper(node.right, ans, height - 1);
+    }
+}
