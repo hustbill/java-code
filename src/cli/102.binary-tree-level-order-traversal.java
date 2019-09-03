4fc0f747b8bd5b5c706e18641d9b91a84c3c6316
hua zhang <huazhang@visa.com>
2019-09-02 18:41:38 -0700

    102.binary-tree-level-order-traversal.java

--- src/cli/102.binary-tree-level-order-traversal.java
+++ src/cli/102.binary-tree-level-order-traversal.java
@@ -47,6 +47,26 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> ret = new LinkedList<List<Integer>>();
+        
+        if (root == null) return ret;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for (int i=0; i<levelNum; i++) {
+                if (queue.peek().left != null) queue.offer(queue.peek().left);
+                if (queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            ret.add(subList);
+         }
+         return ret;
+    }
+
+    public List<List<Integer>> levelOrderi_dfs(TreeNode root) {
         // use dfs, add left child and right child at same height 
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         int height = 0;
