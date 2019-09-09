371b896185ef81bb3bba62ddca60327880c7a6e5
hua zhang <huazhang@visa.com>
2019-09-08 23:01:07 -0700

    python string code

---
+++ src/cli/python/longestSub.py
@@ -0,0 +1,17 @@
+def longestSub(text):
+   idx, cnt, res, max = 0 ,{}, "", -1
+
+   for i in range(len(text)):
+       c = text[i]
+       cnt[c] = cnt.get(c, 0)+1
+       while cnt[c]>2:
+           d = text[idx]
+           idx+=1
+           cnt[d]-=1
+       if max< i-idx:
+           max = i-idx
+           res = text[idx:i+1]
+   return res
+
+ret = longestSub("aaab");
+print(ret);
