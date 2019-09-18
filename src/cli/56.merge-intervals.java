7fa233f3b36574923d8562dc32f54c1519e6d78b
Hua Zhang <hua.zhang@wsu.edu>
2019-09-18 09:39:36 -0700

    56.merge-intervals.java

---
+++ src/cli/56.merge-intervals.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=56 lang=java
+ *
+ * [56] Merge Intervals
+ *
+ * https://leetcode.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (36.38%)
+ * Total Accepted:    407K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * Given a collection of intervals, merge all overlapping intervals.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into
+ * [1,6].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ * 
+ * NOTE: input types have been changed on April 15, 2019. Please reset to
+ * default code definition to get new method signature.
+ * 
+ */
+class Solution {
+    // Sorting takes O(n log(n)) and merging the intervals takes O(n). So, the resulting algorithm takes O(n log(n)).
+    // The idea is to sort the intervals by their starting points. Then, we take the first interval and compare its end with the next intervals starts. As long as they overlap, we update the end to be the max end of the overlapping intervals. Once we find a non overlapping interval, we can add the previous "extended" interval and start over.
+    // To make more memory efficient, I reused the initial array (sort of "in-place") but it would be easy to create new subarrays if you wanted to keep the initial data.
+    public int[][] merge(int[][] intervals) {
+       if (intervals.length <= 1) 
+           return intervals;
+        
+       // Sort by ascending starting point
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+        List<int[]> result = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+
+        for (int[] interval : intervals) {
+            if(interval[0] <= newInterval[1]) // Overlapping intervals, move the end if need
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            else {                           // Disjoint intervals, add the new interval to the list
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
