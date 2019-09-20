fdb311026334c9c9631e7e935dafee6100de1dc3
Hua Zhang <hua.zhang@wsu.edu>
2019-09-19 23:52:18 -0700

    75.sort-colors.java

---
+++ src/cli/75.sort-colors.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ *
+ * https://leetcode.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (42.99%)
+ * Total Accepted:    357K
+ * Total Submissions: 825.2K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * Given an array with n objects colored red, white or blue, sort them in-place
+ * so that objects of the same color are adjacent, with the colors in the order
+ * red, white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red,
+ * white, and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * 
+ * Follow up:
+ * 
+ * 
+ * A rather straight forward solution is a two-pass algorithm using counting
+ * sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then
+ * overwrite array with total number of 0's, then 1's and followed by 2's.
+ * Could you come up with a one-pass algorithm using only constant space?
+ * 
+ * 
+ */
+class Solution {
+       // Complexity Analysis
+       // Time complexity : O(N) since it's one pass along the array of length NN.
+       // Space complexity : O(1) since it's a constant space solution.
+        
+     public void sortColors(int[] nums) {
+        // for all idx < 1: nums[idx < i] = 0
+        // j is an index of element under consideration
+        int p0 = 0, curr = 0;
+        // for all idx > k : nums[idx > k] = 2
+        int p2 = nums.length - 1;
+
+        int tmp;
+        while (curr <= p2) {
+            if (nums[curr] == 0) {
+                // swap p0-th and curr-th elements
+                // i++ and j++
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            } else if (nums[curr] == 2) {
+                // swap k-th and curr-th elements
+                // p2--
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            } else 
+                curr++;
+        }
+    }
+}
