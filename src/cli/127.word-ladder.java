aa2efc38f83829ce385cb90cc00dd3cc2357731b
hustbill <billyzhang2010@gmail.com>
2019-09-28 11:17:12 -0700

    127.word-ladder.java

---
+++ src/cli/127.word-ladder.java
@@ -0,0 +1,123 @@
+/*
+ * @lc app=leetcode id=127 lang=java
+ *
+ * [127] Word Ladder
+ *
+ * https://leetcode.com/problems/word-ladder/description/
+ *
+ * algorithms
+ * Medium (25.14%)
+ * Total Accepted:    302.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
+ *
+ * Given two words (beginWord and endWord), and a dictionary's word list, find
+ * the length of shortest transformation sequence from beginWord to endWord,
+ * such that:
+ * 
+ * 
+ * Only one letter can be changed at a time.
+ * Each transformed word must exist in the word list. Note that beginWord is
+ * not a transformed word.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Return 0 if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ * You may assume no duplicates in the word list.
+ * You may assume beginWord and endWord are non-empty and are not the same.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ * 
+ * Output: 5
+ * 
+ * Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" ->
+ * "dog" -> "cog",
+ * return its length 5.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * beginWord = "hit"
+ * endWord = "cog"
+ * wordList = ["hot","dot","dog","lot","log"]
+ * 
+ * Output: 0
+ * 
+ * Explanation: The endWord "cog" is not in wordList, therefore no possible
+ * transformation.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+import javafx.util.Pair;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // Since all words are of same length.
+    int L = beginWord.length();
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        HashMap<String, ArrayList<String>> allComboDict = new HashMap<String, ArrayList<String>>();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        // Key is the generic word
+                        // Value is a list of words which have the same intermediate generic word.
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1 , L);
+                        ArrayList<String> transformations = 
+                            allComboDict.getOrDefault(newWord, new ArrayList<String>());
+                        transformations.add(word);
+                        allComboDict.put(newWord, transformations);
+                    }
+                });
+        // Queue for BFS
+        Queue<Pair<String, Integer>> Q = new LinkedList<Pair<String, Integer>>();
+        Q.add(new Pair(beginWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+        visited.put(beginWord, true);
+
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < L; i++) {
+                // Intermediate words for current word
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                // Next states are all the words which share the same intermediate state.
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, 
+                            new ArrayList<String>())) {
+                    // If at any point if we find what we are looking for
+                    // i.e. the end word - we can return with the answer.
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    // Otherwise, add it to the BFS Queue. Also mark it visited
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
