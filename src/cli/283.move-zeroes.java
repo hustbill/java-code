2aab2366a031721ea61bcebda07e6884364adb03
hustbill <billyzhang2010@gmail.com>
2019-09-28 23:49:48 -0700

    283.move-zeroes.java

---
+++ src/cli/283.move-zeroes.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ *
+ * https://leetcode.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (54.95%)
+ * Total Accepted:    532.2K
+ * Total Submissions: 963.6K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
+ * Note:
+ * 
+ * 
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * 
+ */
+class Solution {
+     // Approach 1: Two pointer approach
+    // The fast pointer which is denoted by variable "cur" does the job of processing new elements
+    // The slow pointer is the poistion of last found non-0 elment, is denoted by "lastNonFoundAt"
+    // Time : O(N), Space: 0(1)
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non 0 element we found.
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+
+        // After we have finished processing new elements,
+        // all the non-zero elements are alreay at beginning of array.
+        // We just need to fill remaining array with 0's.
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
