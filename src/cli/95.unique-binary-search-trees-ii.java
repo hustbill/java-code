c72070af68ab00903079858e4bc3344b4c470734
hustbill <billyzhang2010@gmail.com>
2019-10-01 00:28:46 -0700

    95.unique-binary-search-trees-ii.java

---
+++ src/cli/95.unique-binary-search-trees-ii.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=95 lang=java
+ *
+ * [95] Unique Binary Search Trees II
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (36.70%)
+ * Total Accepted:    154.2K
+ * Total Submissions: 415.4K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, generate all structurally unique BST's (binary search
+ * trees) that store values 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateSubtrees(1, n);
+    }
+
+    public List<TreeNode> generateSubtrees(int start, int end) {
+        LinkedList<TreeNode> ret = new LinkedList<TreeNode>();
+        if (start > end) {
+            ret.add(null);
+            return ret;
+        }
+
+        // pick up a root
+        for (int i = start; i <= end; i++) {
+            // all possible left substrees if i is choosen to be a root
+            List<TreeNode> leftSubtrees = generateSubtrees(start, i - 1);
+
+            // all possible right substrees if i is choosen to be a root
+            List<TreeNode> rightSubtrees = generateSubtrees(i + 1, end);
+
+            // connect left and right trees to the root i
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    ret.add(root);
+                }
+            }
+        }
+        return ret;
+    }
+
+}
