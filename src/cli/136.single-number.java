c856d4e4293d642aea9955f99182f7e561d2603a
Hua Zhang <hua.zhang@wsu.edu>
2019-10-13 09:27:01 -0700

    136.single-number.java

---
+++ src/cli/136.single-number.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=136 lang=java
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (61.16%)
+ * Total Accepted:    544.5K
+ * Total Submissions: 882.7K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,1]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * 
+ * 
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+       int ret = 0;
+       for (int num : nums) {
+           ret ^= num;
+       }
+       return ret;
+    }
+}
