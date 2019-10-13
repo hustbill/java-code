c07a476c13e957c3198ec1191084184ba05b2867
Hua Zhang <hua.zhang@wsu.edu>
2019-10-12 23:48:59 -0700

    10.regular-expression-matching.java

--- src/cli/10.regular-expression-matching.java
+++ src/cli/10.regular-expression-matching.java
@@ -82,7 +82,56 @@
  * 
  */
 class Solution {
+    /*
+     * 2D DP, time: O(M * N), Space: O(M * N)
+     * https://leetcode.com/problems/regular-expression-matching/discuss/5651/Easy-DP-Java-Solution-with-detailed-Explanation
+     * Here are some conditions to figure out, then the logic can be very straightforward.
+
+        1, If p.charAt(j) == s.charAt(i)  dp[i][j] = dp[i-1][j-1];
+        2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+        3, If p.charAt(j) == '*':
+        here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i)  dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               2   if p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+
+     */
+
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) return false;
+        
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // deals with patterns like a* or a*b* or a*b*c*
+        for (int j = 0; j < n; j++) {
+            if (p.charAt(j) == '*' && dp[0][j - 1])
+                dp[0][j + 1] = true;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } 
+                
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != '.' && p.charAt(j - 1) != s.charAt(i)) {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]) ;
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public boolean isMatch_recursive(String s, String p) {
         if (p.isEmpty())  return s.isEmpty();
 
         boolean firstMatch = (!s.isEmpty() &&
