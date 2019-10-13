d6ba570af38870e157a18513fae2fa5c9a65c5e8
Hua Zhang <hua.zhang@wsu.edu>
2019-10-12 19:27:45 -0700

    10.regular-expression-matching.java

---
+++ src/cli/10.regular-expression-matching.java
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ *
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.58%)
+ * Total Accepted:    349.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression
+ * matching with support for '.' and '*'.
+ * 
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like . or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'.
+ * Therefore, by repeating 'a' once, it becomes "aa".
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore,
+ * it matches "aab".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty())  return s.isEmpty();
+
+        boolean firstMatch = (!s.isEmpty() &&
+                (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+        
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2))) ||
+                    (firstMatch && isMatch(s.substring(1), p));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
