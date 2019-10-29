e5efb8f7349919aace722cb24332bc8240979ba2
hustbill <billyzhang2010@gmail.com>
2019-10-29 14:49:47 -0700

    69.sqrt-x.java

--- src/lc-extensions/69.sqrt-x.java
+++ src/lc-extensions/69.sqrt-x.java
@@ -42,7 +42,22 @@
 // @lc code=start
 class Solution {
     public int mySqrt(int x) {
-        
+        if (x == 0)
+            return 0;
+        int low = 1;
+        int hi = x;
+        while (low + 1 < hi) {
+            int mid = low + (hi - low) / 2;
+            if (mid > x / mid) { // use divide to avoid overflow
+                hi = mid;
+            } else {
+                low = mid;
+            }
+            if (hi <= x / hi) {
+                return hi;
+            }
+        }
+        return low;
     }
 }
 // @lc code=end
