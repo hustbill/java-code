e733b18a3f8d57fa45502e807755f5f830769210
hustbill <billyzhang2010@gmail.com>
2019-11-26 21:58:57 -0800

    src/cli/23.merge-k-sorted-lists.java

---
+++ src/cli/23.merge-k-sorted-lists.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (36.48%)
+ * Total Accepted:    471.6K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
---
+++ src/cli/README.md
@@ -0,0 +1,12 @@
+# LC Command editor
+
+
+## FAQ
+1.["leetcode test" doesn't work anymore ](https://github.com/skygragon/leetcode-cli/issues/192)  
+* Solution: *  
+```bash
+on mac os:
+$ npm -g uninstall leetcode-cli --save
+$ npm -g install leetcode-tools/leetcode-cli
+
+```
