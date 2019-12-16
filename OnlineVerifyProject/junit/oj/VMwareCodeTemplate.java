9977e9946c30287eb2b16380e479e5084bc5a582
hustbill <billyzhang2010@gmail.com>
2019-12-16 15:33:28 -0800

    vmware oddEvensort

---
+++ OnlineVerifyProject/junit/oj/VMwareCodeTemplate.java
@@ -0,0 +1,121 @@
+package oj;
+import java.util.*;
+
+public class VMwareCodeTemplate {
+
+
+
+	public static  void main(String[] args) {
+		String digits = "23";
+		VMwareCodeTemplate cdt  = new VMwareCodeTemplate();
+		
+		int[] inputArr = {1, 2 ,3 ,4 ,5 ,6 , 7 }; //
+		int[] outputArr = cdt.sortArrayByParity(inputArr);
+		for (int i = 0; i < outputArr.length; i++)
+			System.out.print(outputArr[i] + "\t");	
+		// outputArr: 2	4	6	7	5	3	1	
+		
+		System.out.println();
+		
+			// verify_oddEventSort
+	        int[] numbers = new int[]{12,33,45,33,13,55,34,7,6};  
+	        oddEventSort(numbers);  
+	        for(int i=0; i<numbers.length; i++){  
+	            System.out.print(numbers[i]+" ");  
+	        } 
+	}
+	
+	public int[] sortArrayByParity(int[] A) {
+		if(A.length == 0) 
+			return A;
+		
+		int[] B = new int[A.length];
+		
+		int begin = 0;
+		int end = B.length - 1;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] % 2 == 0) {
+				B[begin] = A[i];
+				begin++;
+			} else {
+				B[end] = A[i];
+				end--;
+			}
+		}
+		return B;
+	}
+	
+	/*
+	 *  经典排序算法 - 奇偶排序Odd-even sort
+		又一个比较性质的排序,基本思路是奇数列排一趟序,偶数列排一趟序,再奇数排,再偶数排,直到全部有序
+		
+		举例吧,		
+		待排数组[6 2 4 1 5 9]  => [1 2 4 5 6 9]
+		比较过程如下：
+		
+		第一次比较奇数列,奇数列与它的邻居偶数列比较,如6和2比,4和1比,5和9比
+		
+		[6 2 4 1 5 9]
+		
+		交换后变成
+		
+		[2 6 1 4 5 9]
+		
+		 
+		
+		第二次比较偶数列,即6和1比,4和5比
+		
+		[2 6 1 4 5 9]
+		
+		交换后变成
+		
+		[2 1 6 4 5 9]
+		
+		 
+		
+		第三趟又是奇数列,选择的是2,6,5分别与它们的邻居列比较
+		
+		[2 1 6 4 5 9]
+		
+		交换后
+		
+		[1 2 4 6 5 9]
+		
+		 
+		
+		第四趟偶数列
+		
+		[1 2 4 6 5 9]
+		
+		一次交换
+		
+		[1 2 4 5 6 9]
+		
+		参考:http://modun.iteye.com/blog/263976
+	 */
+	 
+
+	    private static void oddEventSort(int[] numbers){  
+	        int temp;  
+	        for(int i=numbers.length-1; i>numbers.length/2-1; i--){  
+	            for(int j=1; j<=i; j+=2){  
+	                if(j==numbers.length-1)break;  
+	                if(numbers[j]>numbers[j+1]){  
+	                    temp = numbers[j];  
+	                    numbers[j] = numbers[j+1];  
+	                    numbers[j+1] = temp;  
+	                }  
+	            }  
+	            for(int j=0; j<=i; j+=2){  
+	                if(j==numbers.length-1)break;  
+	                if(numbers[j]>numbers[j+1]){  
+	                    temp = numbers[j];  
+	                    numbers[j] = numbers[j+1];  
+	                    numbers[j+1] = temp;  
+	                }  
+	           }  
+	        }  
+	    }  
+	
+
+}
---
+++ src/cli/35.search-insert-position.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (41.24%)
+ * Likes:    1646
+ * Dislikes: 207
+ * Total Accepted:    483.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public int searchInserti_my_ok(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return 0;
+
+        if (nums[0] >= target) return 0;
+
+        if (nums[nums.length - 1] < target) return nums.length;
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return  i;
+            } else {
+                idx = i;
+            }
+        
+        }
+        return idx;
+    }
+}
+// @lc code=end
