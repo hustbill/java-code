cb3a0ce146658699df2452a1dc941f9c1bf02ae8
Hua Zhang <hua.zhang@wsu.edu>
2020-01-05 10:09:21 -0800

    merge .classpath and run at my Mac

---
+++ src/REAME.md
@@ -0,0 +1,12 @@
+# REAME.md
+## Validate the .classpath 
+before import this project java-oj into Eclipse, we need to validate the content of .classpath. 
+
+* 2020-1-05   
+There is an issue on my .classpath, it is a merge info in this file.  
+Ref to link [Eclipse “Error: Could not find or load main class”](https://stackoverflow.com/questions/11235827/eclipse-error-could-not-find-or-load-main-class/27488594)
+
+## Solution:
+fix the merge, then the package  can be running now.
+
+
---
+++ src/lc-extensions/93.restore-ip-addresses.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=93 lang=java
+ *
+ * [93] Restore IP Addresses
+ *
+ * https://leetcode.com/problems/restore-ip-addresses/description/
+ *
+ * algorithms
+ * Medium (32.65%)
+ * Likes:    828
+ * Dislikes: 354
+ * Total Accepted:    156.6K
+ * Total Submissions: 478.6K
+ * Testcase Example:  '"25525511135"'
+ *
+ * Given a string containing only digits, restore it by returning all possible
+ * valid IP address combinations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        
+    }
+}
+// @lc code=end
+
